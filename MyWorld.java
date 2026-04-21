d8677810bdf457b5d6f5ac593ef0f3e2451938cb
KingBobSkibidi <lucianbai0808@gmail.com>
2026-04-20 21:18:30 -0400

    April 20: fixed files in folder/organization
    - fixed files in folder/organization so Lucian can open in green foot properly

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,25 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class MyWorld extends World {
-    public MyWorld() {
-        super(600, 400, 1);
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        Baldy baldy = new Baldy();
+        addObject(baldy, 100, 100);
+        GreenApple greenapple = new GreenApple();
+        addObject(greenapple, 600, 100);
     }
 }
