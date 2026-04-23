e7b8d4d5bbcab1f8f309661862277d32a04bc114
KingBobSkibidi <lucianbai0808@gmail.com>
2026-04-23 10:04:32 -0400

    April 23: completed Defeated class
    - completed defeated class, just setting the defeat image

--- Defeated.java
+++ Defeated.java
@@ -12,6 +12,13 @@ public class Defeated extends Actor
      * Act - do whatever the defeat wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public Defeated() {
+        GreenfootImage defeatedImage = new GreenfootImage("defeated.jpg");
+        defeatedImage.scale(200, 200);
+        setImage(defeatedImage);
+    }
+    
     public void act()
     {
         // Add your action code here.
