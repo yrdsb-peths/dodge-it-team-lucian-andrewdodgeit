2ccc9bfdcbad929e7f93f641e1d6c636bd0ae1d6
KingBobSkibidi <lucianbai0808@gmail.com>
2026-04-23 10:03:37 -0400

    April 23: completed villain class
    - completed villain class with randomly spawning villain that moves towards hero for hero to dodge
    - renamed Defeat subclass to Defeated

--- Defeated.java
+++ Defeated.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Defeat extends Actor
+public class Defeated extends Actor
 {
     /**
      * Act - do whatever the defeat wants to do. This method is called whenever
--- villain.java
+++ villain.java
@@ -12,8 +12,32 @@ public class Villain extends Actor
      * Act - do whatever the villain wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Villain(){   
+        GreenfootImage villainImage = new GreenfootImage("kingpin.jpg");
+        villainImage.scale(50, 50);
+        setImage(villainImage);
+    }
+    
     public void act()
     {
-        // Add your action code here.
+        move(-5);
+        if (getX() <= 50) {
+            resetVillain();
+        }
+        
+        if (isTouching(Hero.class)) {
+            getWorld().addObject(new Defeated(), 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetVillain() {
+        int random = Greenfoot.getRandomNumber(2);
+        if (random == 0) {
+            setLocation(600, 100);
+        }
+        else {
+            setLocation(600, 300);
+        }
     }
 }
