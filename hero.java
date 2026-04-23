7a87ed0b5447fdede66c4e4d8a7da40a585fc1e8
KingBobSkibidi <lucianbai0808@gmail.com>
2026-04-23 10:00:32 -0400

    April 23: Completed hero class
    - completed hero class, making hero go up/down swapping between 2 positions when you click
    - changed all subclass names to capitalized

--- hero.java
+++ hero.java
@@ -6,14 +6,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class hero extends Actor
+public class Hero extends Actor
 {
     /**
      * Act - do whatever the hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Hero(){
+        GreenfootImage heroImage = new GreenfootImage("spiderman.jpg");
+        heroImage.scale(50, 50);
+        setImage(heroImage);
+    }
+    
+    boolean isAtTop = true;
+    
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(null)){
+            if(isAtTop == false) {
+                setLocation(100, 100);
+                isAtTop = true;
+            }
+            else{
+                setLocation(100, 300);
+                isAtTop = false;
+            }
+        }
     }
 }
